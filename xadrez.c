40f48f030db1094e96f4acaec6a5f0e2bf7e8eb1
DaviAguilar07 <aguilardavi44@gmail.com>
2026-04-22 18:30:06 +0000

    Completei o desafio intermediario

--- xadrez.c
+++ xadrez.c
@@ -7,17 +7,18 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int i = 1, numero = 5;
+    int i = 1, numero = 5, movimentocavalo = 1, andarcavalo = 31;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    printf("Movimentação ordenada ao bispo: \n");
+    printf("Movimentação ordenada ao bispo (5 casas em diagonal): \n");
         while ( i <= 5)
         {
             printf("Cima e direita\n");
         i++;
         }
         
+        printf("\n");
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
        do{
@@ -31,16 +32,39 @@ int main() {
             printf("Você digitou %d\n", numero);
         }
        }while( numero != 5);
+      
+       printf("\n");
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-       printf("A movimentação da rainha:\n");
+       printf("A movimentação da rainha (8 casas para a esquerda):\n");
         for(int e = 1; e <= 8; e++){
         printf("Esquerda\n");
        }
+
+       printf("\n");
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+       do{
+       printf("Digite '31' para o cavalo andar três vezes para baixo e uma vez para a esquerda.\n");
+       scanf("%d", &andarcavalo);
 
+       if(andarcavalo == 31){
+        printf("Você moveu o cavalo.\n");
+
+        while(movimentocavalo--){
+            for(int m = 1; m <= 3; m++){
+                printf("Baixo\n");
+            }
+            printf("Esquerda\n");
+            }
+        break;
+
+        }else{
+        printf("Você digitou %d, tente novamente.\n", andarcavalo);
+       }
+    }while(1);
+       
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
