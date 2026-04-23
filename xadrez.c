8418261a308a51b6626da9b6a7d7a42b078cae91
DaviAguilar07 <aguilardavi44@gmail.com>
2026-04-23 18:24:37 +0000

    Completei o desafio mestre

--- xadrez.c
+++ xadrez.c
@@ -4,20 +4,44 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void recursivorainha(int rainha){
+    if(rainha > 0){
+    printf("Esquerda\n");
+    recursivorainha(rainha - 1);
+
+    }
+}
+
+void recursivotorre(int torre){
+    if(torre > 0){
+        printf("Direita\n");
+        recursivotorre(torre - 1);
+    }
+}
+
+void recursivobispo(int bispo) {
+    for (int v = bispo; v >= 1; v--) { // movimento vertical decrescente
+        for (int h = bispo; h >= 1; h--) { // movimento horizontal decrescente
+            if (v == h) { // garante que seja diagonal (mesma quantidade de cima e direita)
+                printf("Cima\n");
+                printf("Direita\n");
+            }
+        }
+    }
+}
+
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int i = 1, numero = 5, movimentocavalo = 1, andarcavalo = 31;
+    int i = 1, numero = 5, movimentocavalo = 1, andarcavalo = 21;
+    int quantidaderainha = 8, quantidadetorre = 5, quantidadebispo = 5;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    printf("Movimentação ordenada ao bispo (5 casas em diagonal): \n");
-        while ( i <= 5)
-        {
-            printf("Cima e direita\n");
-        i++;
-        }
-        
+    printf(" Movimentação do Bispo: \n");
+    recursivobispo(quantidadebispo);
+    
         printf("\n");
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
@@ -26,8 +50,8 @@ int main() {
         scanf("%d", &numero);
        
         if(numero == 5){
-            for(int j = 1; j <= 5; j++)
-            printf("Direita\n");
+            printf("A movimentação da torre: \n");
+            recursivotorre(quantidadetorre);
         }else{
             printf("Você digitou %d\n", numero);
         }
@@ -37,30 +61,31 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
        printf("A movimentação da rainha (8 casas para a esquerda):\n");
-        for(int e = 1; e <= 8; e++){
-        printf("Esquerda\n");
-       }
+        recursivorainha(quantidaderainha);
 
        printf("\n");
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
        do{
-       printf("Digite '31' para o cavalo andar três vezes para baixo e uma vez para a esquerda.\n");
+       printf("Digite '21' para o cavalo andar duas vezes para cima e uma vez para a direita.\n");
        scanf("%d", &andarcavalo);
 
-       if(andarcavalo == 31){
+       if(andarcavalo == 21){
         printf("Você moveu o cavalo.\n");
 
-        while(movimentocavalo--){
-            for(int m = 1; m <= 3; m++){
-                printf("Baixo\n");
+        for(int cv = 2; cv >= 1; cv--){
+            if(cv == 1){
+            printf("Cima\n");
+            continue; //Seria printado 2 vezes o "Cima", mas o 'continue' eliminou 1 "Cima".
             }
-            printf("Esquerda\n");
-            }
-        break;
-
-        }else{
+            printf("Cima\n");
+        }
+        for(int o = 1; o >= 1; o--){
+            printf("Direita\n");}
+        break; 
+        }
+        else{
         printf("Você digitou %d, tente novamente.\n", andarcavalo);
        }
     }while(1);
